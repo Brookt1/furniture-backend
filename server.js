3eafd2173f2a7c98d24fb48aadc3de0a2a7a5cf2
bb <birukdesalech@gmail.com>
2024-11-12 22:48:22 +0300

    cors add

--- server.js
+++ server.js
@@ -1,26 +1,25 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const initializeDatabase = require('./config/initDb');
-const db = require('./config/db');
-const furnitureRoutes = require('./routes/furnitureRoutes');
-const categoryRoutes = require('./routes/categoryRoutes');
+const express = require("express");
+const dotenv = require("dotenv");
+const initializeDatabase = require("./config/initDb");
+const db = require("./config/db");
+const furnitureRoutes = require("./routes/furnitureRoutes");
+const categoryRoutes = require("./routes/categoryRoutes");
+const cors = require("cors");
 
 dotenv.config();
 const app = express();
 app.use(express.json());
+app.use(cors());
 initializeDatabase();
 
-
-app.get('/', (req, res) => {
-    res.send('Hello There!');
+app.get("/", (req, res) => {
+  res.send("Hello There!");
 });
 
-
-app.use('/api/furniture', furnitureRoutes);
-app.use('/api/categories', categoryRoutes);
-
+app.use("/api/furniture", furnitureRoutes);
+app.use("/api/categories", categoryRoutes);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT,() =>{
-    console.log(`Server is running on port ${PORT}`);
-})
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
