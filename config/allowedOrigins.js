acef90b88ca58c47d9e9f71b95d6aa4940871a2e
bb <birukdesalech@gmail.com>
2025-02-16 21:16:47 +0300

    add new allowed origin

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -3,6 +3,7 @@ const allowedOrigins = [
   "http://127.0.0.1:3000",
   "http://localhost:5173",
   "https://furnitureapi-ykrq.onrender.com",
+  "https://furniture-backend.duckdns.org",
   "https://europena-blue.vercel.app",
   "https://european-luxury.netlify.app",
   "https://europena.vercel.app",
